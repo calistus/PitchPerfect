1d999881f641b6688778fd410023ca727c17e490
calistus <icalistus@gmail.com>
2017-04-25 08:27:33 +0100

    add stop recording Button

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,10 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var recordingLabel: UIButton!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -23,6 +20,12 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(_ sender: Any) {
         print("Record button was pressed")
+        
+        
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop recording button was pressed")
     }
 }
 
